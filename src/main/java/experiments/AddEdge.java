3547a95cc07a13dcbbcdb0ed22844cd9efbd4f1e
nikhithajosh <nikhitha.josh@ucdconnect.ie>
2023-04-15 23:26:55 +0100

    Generated javadocs for experiments package

--- src/main/java/experiments/AddEdge.java
+++ src/main/java/experiments/AddEdge.java
@@ -8,28 +8,46 @@ import dataStructure.hashMap.LinkedListHashMap;
 import dataStructure.hashMap.TreeHashMap;
 import util.GraphGeneration;
 import util.VanetEntry;
-
 import java.util.List;
 
+/**
+ * The AddEdge class contains the main method that generates and compares different graphs
+ * based on different data structures for storing graph data.
+ * It uses the GraphGeneration utility class to generate Vanet data for each number of vertices.
+ */
 public class AddEdge {
+    /**
+     * The main method that generates and compares different graphs based on different data structures
+     * for storing graph data. It uses the GraphGeneration utility class to generate Vanet data for each
+     * number of vertices.
+     *
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
+
+        // Array of different number of vertices
         int[] vertices = {10, 50, 100, 500, 1000, 2500, 5000};
+
+        // Loop through each number of vertices and generate graphs based on different data structures
         for (int i = 0; i < vertices.length; i++) {
             List<VanetEntry> vanetData = GraphGeneration.generateVanetData(vertices[i]);
             System.out.println("To store " + vertices[i] + " vehicles and " + vanetData.size() + " edges:");
 
+            // Graph based on LinkedListHashMap
             long start = System.currentTimeMillis();
             HashMap<Vehicle, HashMap<Vehicle, Integer>> linkedListHashMap = new LinkedListHashMap<>();
             Graph<Vehicle> linkedListHashMapGraph = new HashMapGraph<>(linkedListHashMap);
             GraphGeneration.createGraph(linkedListHashMapGraph, vanetData);
             System.out.println("HashMap Graph based on Linked List took: " + (System.currentTimeMillis() - start) + "ms");
 
+            // Graph based on TreeHashMap
             start = System.currentTimeMillis();
             HashMap<Vehicle, HashMap<Vehicle, Integer>> treeHashMap = new TreeHashMap<>();
             Graph<Vehicle> treeHashMapGraph = new HashMapGraph<>(treeHashMap);
             GraphGeneration.createGraph(treeHashMapGraph, vanetData);
             System.out.println("HashMap Graph based on Tree took: " + (System.currentTimeMillis() - start) + "ms");
 
+            // Graph based on AdjacencyListGraph
             start = System.currentTimeMillis();
             Graph<Vehicle> adjacencyListGraph = new AdjacencyListGraph<>();
             GraphGeneration.createGraph(adjacencyListGraph, vanetData);
@@ -38,5 +56,4 @@ public class AddEdge {
             System.out.println();
         }
     }
-
 }
--- src/main/java/experiments/DeleteEdge.java
+++ src/main/java/experiments/DeleteEdge.java
@@ -1,5 +1,4 @@
 package experiments;
-
 import dataStructure.graph.Graph;
 import dataStructure.graph.adjacencyListGraph.AdjacencyListGraph;
 import dataStructure.graph.hashMapGraph.HashMapGraph;
@@ -8,23 +7,34 @@ import dataStructure.hashMap.LinkedListHashMap;
 import dataStructure.hashMap.TreeHashMap;
 import util.GraphGeneration;
 import util.VanetEntry;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * A class to test the performance of deleting edges from different types of graphs.
+ */
 public class DeleteEdge {
+    // A random number generator
     static Random random = new Random();
 
+    /**
+     * The main method of the class
+     *
+     * @param args the command line arguments (not used in this program)
+     */
     public static void main(String[] args) {
+        // An array with the number of vertices to be used in each test
         int[] vertices = {10, 50, 100, 500, 1000, 2500, 5000};
         for (int i = 0; i < vertices.length; i++) {
+            // Generate the VANET data
             List<VanetEntry> vanetData = GraphGeneration.generateVanetData(vertices[i]);
+            // Select the edges to be deleted
             List<VanetEntry> edgesToBeDeleted = getEdgesToBeDeleted(vanetData, (int) (vanetData.size() * 0.33));
             System.out.println("To delete " + edgesToBeDeleted.size() + " edges in graph of " + vertices[i]
                     + " vehicles and " + vanetData.size() + " edges:");
 
-
+            // Test the performance of deleting edges from a graph based on a linked list hash map
             HashMap<Vehicle, HashMap<Vehicle, Integer>> linkedListHashMap = new LinkedListHashMap<>(16, false);
             Graph<Vehicle> linkedListHashMapGraph = new HashMapGraph<>(linkedListHashMap);
             GraphGeneration.createGraph(linkedListHashMapGraph, vanetData);
@@ -34,6 +44,7 @@ public class DeleteEdge {
             }
             System.out.println("HashMap Graph based on Linked List took: " + ((double) (System.nanoTime() - start) / 1000000) + "ms");
 
+            // Test the performance of deleting edges from a graph based on a tree hash map
             HashMap<Vehicle, HashMap<Vehicle, Integer>> treeHashMap = new TreeHashMap<>(16, false);
             Graph<Vehicle> treeHashMapGraph = new HashMapGraph<>(treeHashMap);
             GraphGeneration.createGraph(treeHashMapGraph, vanetData);
@@ -43,6 +54,7 @@ public class DeleteEdge {
             }
             System.out.println("HashMap Graph based on Tree took: " + ((double) (System.nanoTime() - start) / 1000000) + "ms");
 
+            // Test the performance of deleting edges from an adjacency list graph
             Graph<Vehicle> adjacencyListGraph = new AdjacencyListGraph<>();
             GraphGeneration.createGraph(adjacencyListGraph, vanetData);
             start = System.nanoTime();
@@ -54,6 +66,13 @@ public class DeleteEdge {
             System.out.println();
         }
     }
+    /**
+     * This method takes a list of VanetEntry objects and an integer number of edges to be deleted, and returns
+     * a list of VanetEntry objects representing edges that will be deleted from the graph.
+     * @param vanetData A list of VanetEntry objects representing edges in the graph
+     * @param numberOfEdges An integer number of edges to be deleted from the graph
+     * @return A list of VanetEntry objects representing edges to be deleted from the graph
+     */
 
     static List<VanetEntry> getEdgesToBeDeleted(List<VanetEntry> vanetData, int numberOfEdges) {
         List<VanetEntry> edgesToBeDeleted = new ArrayList<>();
